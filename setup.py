c9e56b44f9385a6d8c88196e6083a490db524a59
Andrey Kan <andrey.kan@dowjones.com>
2023-03-01 13:56:22 -0500

    setup

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
  
-setup(name='spotassist-djoser',
-    version='1.0.5',
+setup(name='djoser',
+    version='1.0.6',
     description='Spotassist Djoser',
     url='https://github.com/tevch/djoser.git',
     author='Andrey Kan',
     author_email='list@livewings.com',
     license='MIT',
+    packages=['djoser'],
     zip_safe=False)
