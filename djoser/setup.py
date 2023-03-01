b00b70c5899e5bc0b2194dc833f8c9ddcaac05ee
Andrey Kan <andrey.kan@dowjones.com>
2023-03-01 13:30:12 -0500

    no check for password reset

---
+++ djoser/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+ 
+setup(name='djoser',
+    version='1.0.0',
+    description='Spotassist Djoser',
+    url='https://github.com/tevch/djoser.git',
+    author='Andrey Kan',
+    author_email='list@livewings.com',
+    license='MIT',
+    packages=['djoser'],
+    zip_safe=False)
